4e77b4492155d5faba4be948b4ec5812441fc370
Matt Havener <matthavener@gmail.com>
2017-02-08 15:08:11 -0600

    Workaround lack of clojure.main

--- env/dev/user.clj
+++ env/dev/user.clj
@@ -1,18 +1,12 @@
 (ns user
   (:require
-    [cljs.build.api :as cljsbuild]))
-
-#_(Thread/setDefaultUncaughtExceptionHandler
- (reify Thread$UncaughtExceptionHandler
-   (uncaughtException [_ thread ex]
-     (println "uncaught exception: " (.getMessage ex) (.getName thread))
-     (.printStackTrace ex)
-     (log/error ex "Uncaught exception on thread: " (.getName thread)))))
-
+    [clojure.main]
+    [cljs.build.api :as cljsbuild]
+    [om.next :as om]
+    ))
 
 (defn doit []
   (println "building")
-  (require '[om.next :as om])
   (cljsbuild/build (cljsbuild/ns->source 'testcase.web)
                    {:output-to "testcase.js"
                     :main 'testcase.web
